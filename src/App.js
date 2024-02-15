1dde0a89b1178e9b1f1541d9b741fedbb6982071
Pritam Manna <pritammanna814@gmail.com>
2024-02-15 23:52:08 +0530

    add new pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
 import "./index.css";
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
+      <About />
+      <Skill />
+      <Work />
+      <Contact />
     </>
   );
 }
