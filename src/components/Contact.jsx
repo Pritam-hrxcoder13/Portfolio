a22bbec5ec74e3b4a90a25a281f5b9fd92bd61b4
Pritam Manna <pritammanna814@gmail.com>
2024-02-19 15:28:03 +0530

    add contact form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = () => {
                     <p className='text-gray-300 py-4'>Submit the below form or shoot me an email - pritamanna814@gmail.com</p>
 
                 </div>
-                <input type="text" placeholder='Name' name='name' className='bg-[#ccdcf6] p-2' />
-                <input type="text" placeholder='Email' name='email' className='bg-[#ccdcf6] p-2 my-4' />
-                <textarea name="message" className='bg-[#ccdcf6] p-2' placeholder='Message' rows="10"></textarea>
+                <input type="text" placeholder='Name' name='name' className='bg-[#ccdcf6] p-2' required />
+                <input type="text" placeholder='Email' name='email' className='bg-[#ccdcf6] p-2 my-4' required />
+                <textarea name="message" className='bg-[#ccdcf6] p-2' placeholder='Message' rows="10" required></textarea>
                 <button className='text-white group border-2 px-4 py-3 my-8 mx-auto flex items-center hover:bg-[#ffae4ae3] hover:border-[#ffae4ae3]'>Let's Collaborate</button>
             </form>
         </div>
