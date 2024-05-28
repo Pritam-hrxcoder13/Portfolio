0fd7259d123059b5be4f5f032145ef85513948c2
Pritam Manna <pritammanna814@gmail.com>
2024-05-29 00:58:18 +0530

    modify server.js

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Ensure correct MIME type for CSS files
-app.use('/static/css', express.static(path.join(__dirname, 'build/static/css'), {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.css')) {
-            res.set('Content-Type', 'text/css');
-        }
-    }
-}));
-
-// Catch-all handler to return the main index.html file for any route
+// Fallback to index.html for other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    const filePath = path.join(__dirname, 'build', req.path);
+    if (req.path.startsWith('/static') || req.path.endsWith('.js') || req.path.endsWith('.css') || req.path.endsWith('.json')) {
+        res.sendFile(filePath, (err) => {
+            if (err) {
+                res.sendFile(path.join(__dirname, 'build', 'index.html'));
+            }
+        });
+    } else {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    }
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
